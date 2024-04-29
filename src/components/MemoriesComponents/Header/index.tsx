d23a45e483862df0756da6a907018b04f02fa839
aziz-sassi50 <aziz.sassi@youree.io>
2024-04-29 20:33:27 +0100

    fixed icon

--- src/components/MemoriesComponents/Header/index.tsx
+++ src/components/MemoriesComponents/Header/index.tsx
@@ -11,12 +11,12 @@ const Header: React.FC = () => {
                 <View style={{ flexDirection: 'row', flex: 1, alignItems: 'center', backgroundColor: 'white', borderRadius: 8, borderWidth: 1, borderColor: '#e0e0e0', paddingHorizontal: 12, height: 40, width : "100%" }}>
                     {/* Text input */}
                     <Ionicons size={20} color="grey" name="search-outline"></Ionicons>
-                    <TextInput placeholder="Search" style={{ flex: 1 }} />
+                    <TextInput placeholder="Search"  style={{ flex: 1 }} />
                     
                     {/* Icons container */}
                     <View style={{ flexDirection: 'row' }}>
                         <TouchableOpacity style={styles.icon}>
-                            <Ionicons name="filter" size={24} color="black" />
+                            <Ionicons name="color-filter-outline" size={24} color="black" />
                         </TouchableOpacity>
                         <TouchableOpacity style={styles.icon}>
                             <Ionicons name="add" size={24} color="black" />
