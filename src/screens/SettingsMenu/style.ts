69b7bf42da8a009e28a2e740197b6494f29c8df3
fady_bel_y <fady.dekhil@gmail.com>
2024-05-11 12:07:53 +0100

    Full Static Settings Screen and changed bottom navigation bg color and shadow to focused

--- src/screens/SettingsMenu/style.ts
+++ src/screens/SettingsMenu/style.ts
@@ -6,7 +6,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFD665',
   },
   transparentHolder: {
-    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    backgroundColor: 'rgba(255, 255, 255, 0.75)',
   },
   item: {
     flexDirection: 'row',
